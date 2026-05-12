5cadc14b3c7d6b94c1c68740670da1a2be548559
os3116os93-cyber <os3116os93@gmail.com>
2026-05-12 15:20:13 +0900

    spec.py

--- spec.py
+++ spec.py
@@ -135,8 +135,8 @@ st.markdown("""
     border: none !important;
     border-radius: 0 0 14px 14px !important;
     font-weight: 700 !important;
-    font-size: 14px !important;
-    height: 46px !important;
+    font-size: 11px !important;
+    height: 40px !important;
     width: 100% !important;
     margin: 0 !important;
     padding: 0 !important;
